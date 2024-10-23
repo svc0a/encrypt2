f44cd268f603810644a9f01686f5640a62c0a896
owen <owen@gmail.com>
2024-10-23 16:43:36 +0800

    update

--- map2/service.go
+++ map2/service.go
@@ -10,13 +10,11 @@ type Data[T any] struct {
 }
 
 func Sorted[T any](params map[string]T) []Data[T] {
-	// 将参数按照键名排序
 	keys := make([]string, 0, len(params))
 	for key := range params {
 		keys = append(keys, key)
 	}
 	sort.Strings(keys)
-	// 按照指定格式拼接键值对
 	arr := []Data[T]{}
 	for _, key := range keys {
 		arr = append(arr, Data[T]{
